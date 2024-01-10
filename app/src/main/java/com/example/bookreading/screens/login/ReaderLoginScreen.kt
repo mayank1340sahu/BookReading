72f0e97895fb289bde65325964e06f0be7655262
Mayank Sahu <mayanksahu34753@gmail.com>
2024-01-10 19:07:36 +0530

    all done

--- app/src/main/java/com/example/bookreading/screens/login/ReaderLoginScreen.kt
+++ app/src/main/java/com/example/bookreading/screens/login/ReaderLoginScreen.kt
@@ -1,17 +1,67 @@
 package com.example.bookreading.screens.login
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 
+@OptIn(ExperimentalComposeUiApi::class)
 @Composable
 fun ReaderLoginScreen(navController: NavHostController) {
+    val email = rememberSaveable {
+        mutableStateOf("")
+    }
+    val password = rememberSaveable {
+        mutableStateOf("")
+    }
+    val passwordVisibility = rememberSaveable {
+        mutableStateOf(false)
+    }
+    val passwordFocusRequest = FocusRequester.Default
+
+    val keyBoardController = LocalSoftwareKeyboardController.current
+
+    val valid = rememberSaveable(email.value,password.value) {
+         email.value.trim().isNotEmpty() && password.value.trim().isNotEmpty()
+     }
+    val modifier = Modifier
+        .height(34.dp)
+        .verticalScroll(rememberScrollState())
+        .background(MaterialTheme.colorScheme.background)
     Column(Modifier.fillMaxWidth(),
         horizontalAlignment = Alignment.CenterHorizontally,
-        ){ Text(text = "Login Screen") }
+        ){
+
+    }
+}
+
+@Composable
+fun EmailInput(modifier : Modifier = Modifier,
+               emailState : MutableState<String>,
+               labelId : String = "Email",
+               enable : Boolean = true,
+               imeAction : ImeAction = ImeAction.Next,
+               onAction : KeyboardActions = KeyboardActions.Default
+               ) {
+    inputField()
+}
+
+fun inputField() {
 
 }
