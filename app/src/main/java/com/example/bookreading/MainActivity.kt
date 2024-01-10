f7b7079c2eee7476a1b79bd8873700f33f96271b
Mayank Sahu <mayanksahu34753@gmail.com>
2024-01-10 11:58:36 +0530

    all done

--- app/src/main/java/com/example/bookreading/MainActivity.kt
+++ app/src/main/java/com/example/bookreading/MainActivity.kt
@@ -18,8 +18,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             BookReadingTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background) {
+                Surface() {
                     ReaderNavigation()
                 }
             }
--- app/src/main/java/com/example/bookreading/screens/ReaderSplashScreen.kt
+++ app/src/main/java/com/example/bookreading/screens/ReaderSplashScreen.kt
@@ -1,9 +1,66 @@
 package com.example.bookreading.screens
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.LinearOutSlowInEasing
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.spring
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.navigation.NavHostController
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import kotlinx.coroutines.delay
 
+@Preview
 @Composable
-fun ReaderSplashScreen(navController: NavHostController) {
-    Text("Splash Screen")
+fun ReaderSplashScreen(navController: NavController = NavController(context = LocalContext.current)) {
+    val scale = remember{
+      Animatable(0f)
+    }
+    LaunchedEffect(key1 = true){
+        scale.animateTo(targetValue = 0.9f,
+            animationSpec = tween(durationMillis = 800,
+                easing = LinearOutSlowInEasing ),
+            )
+       delay(800)
+    }
+    Column(Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally){
+        Surface(
+            Modifier
+                .scale(scale.value)
+                .clip(CircleShape)
+                .size(300.dp)
+                .border(3.dp, Color.Red, shape = CircleShape)
+        ) {
+            Column(
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(
+                    text = "Book Reading",
+                    style = MaterialTheme.typography.headlineMedium,
+                    color = Color(0xFFD62E67)
+                )
+                Text(text = "\"Reading must be a habit\"", color = Color.DarkGray)
+            }
+        }
+    }
 }
