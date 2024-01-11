83799c104ea0adb9b1d2f69226baba7f2496af3f
Mayank Sahu <mayanksahu34753@gmail.com>
2024-01-11 19:02:10 +0530

    all done

--- app/src/main/java/com/example/bookreading/screens/login/ReaderLoginScreen.kt
+++ app/src/main/java/com/example/bookreading/screens/login/ReaderLoginScreen.kt
@@ -1,13 +1,22 @@
 package com.example.bookreading.screens.login
 
+import android.util.Log
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
@@ -16,14 +25,36 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 
-@OptIn(ExperimentalComposeUiApi::class)
+@Preview
 @Composable
-fun ReaderLoginScreen(navController: NavHostController) {
+fun ReaderLoginScreen(
+    navController: NavHostController = NavHostController(context = LocalContext.current)) {
+   UserForm(loading = false, isCreatedAccount = false){ email,pass ->
+       Log.d("user Form", "ReaderLoginScreen: $email,$pass")
+   }
+}
+
+@OptIn(ExperimentalComposeUiApi::class)
+@Preview
+@Composable
+fun UserForm(
+    loading : Boolean = false,
+    isCreatedAccount : Boolean = false,
+    onDone : (String,String) -> Unit = { _, _ ->}
+) {
     val email = rememberSaveable {
         mutableStateOf("")
     }
@@ -38,16 +69,28 @@ fun ReaderLoginScreen(navController: NavHostController) {
     val keyBoardController = LocalSoftwareKeyboardController.current
 
     val valid = rememberSaveable(email.value,password.value) {
-         email.value.trim().isNotEmpty() && password.value.trim().isNotEmpty()
-     }
+        email.value.trim().isNotEmpty() && password.value.trim().isNotEmpty()
+    }
     val modifier = Modifier
         .height(34.dp)
         .verticalScroll(rememberScrollState())
         .background(MaterialTheme.colorScheme.background)
     Column(Modifier.fillMaxWidth(),
         horizontalAlignment = Alignment.CenterHorizontally,
-        ){
-
+    ){
+        EmailInput(emailState = email, onAction = KeyboardActions {
+            passwordFocusRequest.requestFocus()
+        })
+        PasswordInput(passwordState = password,
+            modifier = modifier.focusRequester(passwordFocusRequest),
+            labelId = "password",
+            enabled = true, //Todo Change this
+            passwordVisibility = passwordVisibility,
+            onAction = KeyboardActions {
+                if (!valid) return@KeyboardActions
+                onDone(email.value.trim(),password.value.trim())
+            }
+        )
     }
 }
 
@@ -59,9 +102,75 @@ fun EmailInput(modifier : Modifier = Modifier,
                imeAction : ImeAction = ImeAction.Next,
                onAction : KeyboardActions = KeyboardActions.Default
                ) {
-    inputField()
+    InputField(valueState = emailState,
+        labelId = labelId,
+        enable = enable,
+        imeAction = imeAction,
+        onAction = onAction,
+        modifier = modifier
+        )
 }
 
-fun inputField() {
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun InputField(
+    modifier : Modifier = Modifier,
+    valueState : MutableState<String>,
+    labelId : String,
+    enable : Boolean,
+    isSingleLine : Boolean = true,
+    keyboardType: KeyboardType = KeyboardType.Text,
+    imeAction : ImeAction,
+   onAction: KeyboardActions = KeyboardActions.Default
+) {
+OutlinedTextField(value = valueState.value,
+    onValueChange = { valueState.value = it },
+    label = { Text(text = labelId)},
+    singleLine = isSingleLine,
+    textStyle = TextStyle(fontSize = 18.sp, color = MaterialTheme.colorScheme.onBackground),
+    modifier = modifier
+        .padding(start = 10.dp, end = 10.dp, bottom = 10.dp)
+        .fillMaxWidth(),
+        enabled = enable,
+    keyboardOptions = KeyboardOptions(keyboardType = keyboardType, imeAction = imeAction),
+    keyboardActions = onAction,
+)
+}
 
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun PasswordInput(
+    passwordState: MutableState<String>,
+    modifier: Modifier,
+    labelId: String,
+    enabled: Boolean,
+    imeAction: ImeAction = ImeAction.Done,
+    passwordVisibility: MutableState<Boolean>,
+    onAction: KeyboardActions = KeyboardActions.Default
+) {
+    val visualTransformation = if(passwordVisibility.value) VisualTransformation.None
+    else {
+        PasswordVisualTransformation()
+    }
+    OutlinedTextField(value = passwordState.value,
+        onValueChange = {passwordState.value = it},
+        enabled = enabled,
+        label = { Text(text = labelId)},
+        modifier = modifier
+            .padding(start = 10.dp, end = 10.dp, bottom = 10.dp),
+        textStyle = TextStyle(fontSize = 18.sp,
+            color = MaterialTheme.colorScheme.onBackground),
+        keyboardOptions = KeyboardOptions(imeAction = imeAction, keyboardType = KeyboardType.Text),
+        singleLine = true,
+        visualTransformation = visualTransformation,
+        trailingIcon = {PasswordVisibility(passwordVisibility = passwordVisibility)}
+    )
+}
+
+@Composable
+fun PasswordVisibility(passwordVisibility: MutableState<Boolean>) {
+    val visible = passwordVisibility.value
+    IconButton(onClick = { passwordVisibility.value = !visible }){
+       Icons.Default.Close
+    }
 }
